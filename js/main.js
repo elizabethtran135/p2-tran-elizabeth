4416e8c6fbe80ddff4ae6af06c6ff51d3e5c7db2
Elizabeth Tran <elizabethtran135@gmail.com>
2019-11-10 16:07:01 -0500

    Add read more and alert functionalities

--- js/main.js
+++ js/main.js
@@ -18,3 +18,31 @@ $( ".hamburger" ).show();
 });
 
 });
+//Pop up
+var ext_link =
+document.getElementById("external_link");
+ext_link.addEventListener("click",
+  function(event) {
+    var is_sure = confirm("Do you want to Donate to Teens Go Green?");
+      if (is_sure === false) {
+        event.preventDefault();
+        alert("You have confirmed to not donate and stay on this page");
+      }
+  }, false
+);
+//Read More
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("button1");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
