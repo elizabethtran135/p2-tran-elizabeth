8134fb2a5339e2d32fcfcb713ac23894b672d880
Elizabeth Tran <elizabethtran135@gmail.com>
2019-11-10 17:22:58 -0500

    Attempt API

--- js/main.js
+++ js/main.js
@@ -46,3 +46,16 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+//API
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var apiResult = JSON.parse(this.responseText);
+
+        //Code Dealing With The API Data Goes Here
+      
+
+    }
+};
+xmlhttp.open('GET', 'http://trashnothing.com/api/v1.1/trashnothing-openapi.yaml', true);
+xmlhttp.send();
